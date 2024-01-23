b5644c2b9ef7221b0a997741f6300f0d75aebe1b
Rajusharma2003 <raju2003sharmars3@gmail.com>
2024-01-23 21:55:15 +0530

    complete get courses and courses lectures by the users

--- controllers/courses.controllers.js
+++ controllers/courses.controllers.js
@@ -4,7 +4,8 @@ import AppError from "../utils/appError.utils.js"
 const courses = async function (req , res , next ) {
 
     try {
-    const courses = await Course.find({}).select("-lectures")
+
+    const courses = await Course.find({}).select("-lectures");
 
     if(!courses){
         return next(
@@ -29,6 +30,27 @@ const courses = async function (req , res , next ) {
 
 const getLetcureByCourseId = async function (req , res ,next) {
 
+    try {
+        
+        const {id} = req.params;
+        const courses = await Course.findById(id);
+
+        if(!courses){
+            return next(
+                new AppError('courses lecture in not find' , 400)
+            )
+        }
+
+        req.status(200).json({
+            success : true,
+            message : 'courses lecture is fetch successfully',
+            lectures : courses.lectures
+        })
+    } catch (error) {
+        return next(
+            new AppError(error.message , 500)
+        )
+    }
 }
 
 
--- routers/course.router.js
+++ routers/course.router.js
@@ -1,13 +1,15 @@
 
 import {Router} from 'express'
 import { courses, getLetcureByCourseId } from '../controllers/courses.controllers.js'
+import { isLoggedIn } from '../middlewares/getUser.middleware.js';
 
 const router = Router();
 
+router.route("/").get(courses)
+router.route('/:id').get(isLoggedIn , getLetcureByCourseId)
 
-
-router.get("/" , courses)
-router.get("/" , getLetcureByCourseId)
+// router.get("/" , courses);
+// router.get("/:id" , isLoggedIn, getLetcureByCourseId);
 
 
 export default router
